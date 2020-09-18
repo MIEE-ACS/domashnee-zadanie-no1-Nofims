03ac7b0ce8291de205c320d900e780e18b4d29ce
Nofims <eld170916@icloud.com>
2020-09-18 17:13:05 +0300

    I did homework_1

---
+++ Dz1_var_9/Dz1_var_9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lessons
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Число А: ");
+                double number1 = double.Parse(Console.ReadLine());
+                Console.Write("Число Б: ");
+                double number2 = double.Parse(Console.ReadLine());
+                Console.Write("Число С: ");
+                double number3 = double.Parse(Console.ReadLine());
+                double B = number1;
+                double C = number2;
+                double A = number3;
+                Console.WriteLine($"C = {C}");
+                Console.WriteLine($"A = {A}");
+                Console.WriteLine($"B = {B}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода данных!");
+            }
+        }
+    }
+}
